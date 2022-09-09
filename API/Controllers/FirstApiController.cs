7eff244a49f3d999d82b85b15ae7a20961c1373c
faizu2k01 <faizhaque35@gmail.com>
2022-09-09 11:54:58 +0530

    code pushing

---
+++ API/Controllers/FirstApiController.cs
@@ -0,0 +1,40 @@
+﻿using API.Entities;
+using API.Entities.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FirstApiController : Controller
+    {
+        DataContext _dataContext;
+        public FirstApiController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        [HttpGet("getData")]
+        [ProducesResponseType(typeof(List<AppUser>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> Index()
+        {
+            var datalist =  await _dataContext.User.ToListAsync();
+
+            return Ok(datalist);
+        }
+
+        [HttpGet("get/{id}/data")]
+        [ProducesResponseType(typeof(List<AppUser>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult> data(int id)
+        {
+            return Ok( await _dataContext.User.FindAsync(id));
+        }
+    }
+}
---
+++ API/Migrations/20220901085704_createdb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
